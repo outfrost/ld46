a84c790623c0fbcd3544f5c5535c81d3c450c98f
outfrost <kotlet.bahn@gmail.com>
2020-04-19 03:07:41 +0200

    Player can pick up items

--- player/PlayerCharacter.gd
+++ player/PlayerCharacter.gd
@@ -1,22 +1,39 @@
 extends RigidBody
 
 # Declare member variables here. Examples:
-var carriedItem = null
+export var pickupRadius: float
+var carryingNode: Spatial
+var carriedItem: RigidBody = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	carryingNode = get_node("Carrying") as Spatial
+	print_debug(carryingNode)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 
 func _physics_process(delta):
-	if Input.is_action_pressed("item_pickup"):
-		var minDist = 0
-		var minDistItem = null
-		for item in get_node("/root/Items").get_children():
-			item = item as Spatial
-			var dist = item.global_transform.origin - self.global_transform.origin
-			if minDistItem == null or minDist > dist:
-				minDist = dist
-				minDistItem = item
+	if Input.is_action_just_pressed("item_pickup"):
+		on_item_pickup()
+
+func on_item_pickup():
+	if carriedItem != null:
+		return
+	var minDist = 0.0
+	var minDistItem = null
+	var items = get_node("/root/TestScene/Items")
+	for item in items.get_children():
+		item = item as RigidBody
+		var dist = (item.global_transform.origin - self.global_transform.origin).length()
+		if dist > pickupRadius:
+			continue
+		if minDistItem == null || dist < minDist:
+			minDist = dist
+			minDistItem = item
+	if minDistItem != null:
+		carriedItem = minDistItem
+		items.remove_child(carriedItem)
+		carryingNode.add_child(carriedItem)
+		carriedItem.set_transform(Transform.IDENTITY)
+		carriedItem.sleeping = true
