d00986acae0f2e127b4dbcd7fd0da8a09d8fa6bc
outfrost <kotlet.bahn@gmail.com>
2020-04-19 22:14:13 +0200

    Make Tree consume Items

---
+++ items/FeedItem.gd
@@ -0,0 +1,20 @@
+extends RigidBody
+
+var scene: Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	scene = get_tree().get_root().get_child(0)
+	
+	scene.get_node("Tree").connect("body_entered", self, "on_entered_tree")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func on_entered_tree(body):
+	if body != self:
+		return
+	self.sleeping = true
+	yield(get_tree().create_timer(1.0), "timeout")
+	get_parent().remove_child(self)
